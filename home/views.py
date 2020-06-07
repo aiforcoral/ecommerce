b330f90bae827ad0350b2434daacddf912bcaadc
aiforcoral <aiforcoral@gmail.com>
2020-06-07 10:26:24 +0545

    View of Home page

--- home/views.py
+++ home/views.py
@@ -9,8 +9,13 @@ class BaseView(View):
 
 class HomeView(BaseView):
     def get(self,request):
-        self.view['items'] = Item.objects.all()
+        # self.view['items'] = Item.objects.all()
+        self.view['specil_items'] = Item.objects.filter(labels = 'special').reverse()[0:12]
         self.view['sliders']=Slider.objects.all()
         self.view['category']=Category.objects.all()
         self.view['subcategory'] = SubCategory.objects.all()
+        self.view['add_one'] = Ad.objects.filter(rank=1)
+        self.view['add_second'] = Ad.objects.filter(rank=2)
+        self.view['add_third'] = Ad.objects.filter(rank=3)
+        self.view['add_forth'] = Ad.objects.filter(rank=4)
         return render(self.request,'index.html',self.view)
